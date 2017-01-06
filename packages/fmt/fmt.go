d3702ff97fbfa0eb43e356037e42f7875f4924f3
Nur Rony <pro.nmrony@gmail.com>
2017-01-07 03:29:51 +0600

    named import

---
+++ packages/fmt/fmt.go
@@ -0,0 +1,7 @@
+package myfmt
+
+import "fmt"
+
+func PrintNewln(a ...interface{}) {
+	fmt.Println(a...)
+}
--- packages/greetPackageTest.go
+++ packages/greetPackageTest.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"./greetings"
+	"fmt"
+	myfmt "./fmt"
 )
 
 func main() {
 	fmt.Println(greetings.GreetMe("Hello", "Nur Rony"))
+	myfmt.PrintNewln("Hello","world")
 	// fmt.Println(greetings.greet("Hello")) cannot refer to unexported name greetings.greet
 }
